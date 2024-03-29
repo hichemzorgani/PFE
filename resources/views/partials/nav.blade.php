133065aa404b52420fa129ce5fbb06285d40155a
MALEK16000 <dzmalek160@gmail.com>
2024-03-29 04:22:54 +0100

    navbar done

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,43 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand navbar-light bg-dark">
-        <div class="nav navbar-nav mr-auto">
-            <li class="nav-item nav-link">
-                <a href="{{ route ('ecole.index') }}" class="nav-link active text-white">Gestion des écoles</a>
+        <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="{{ route ('ecole.index') }}" class="nav-link active text-white">Gérer les écoles</a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('affectation.index') }}" class="nav-link text-white">Gestion des directions </a>
+            <li class="nav-item">
+                <a href="{{ route ('affectation.index') }}" class="nav-link text-white">Gérer les directions </a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('affectations.index') }}" class="nav-link text-white">Gestion des départements</a>
+            <li class="nav-item">
+                <a href="{{ route ('affectations.index') }}" class="nav-link text-white">Gérer les départements</a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('encadrant.index') }}" class="nav-link text-white">Gestion des encadrants</a>
+            <li class="nav-item">
+                <a href="{{ route ('encadrant.index') }}" class="nav-link text-white">Gérer les encadrants</a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('universite.index') }}" class="nav-link text-white">Gestion des universités</a>
+            <li class="nav-item">
+                <a href="{{ route ('universite.index') }}" class="nav-link text-white">Gérer les universités</a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('compte.index') }}" class="nav-link text-white">Gestion des comptes</a>
+            <li class="nav-item">
+                <a href="{{ route ('compte.index') }}" class="nav-link text-white">Gérer les comptes</a>
             </li>
-            <li class="nav-item nav-link">
+            <li class="nav-item">
                 <a href="{{ route ('statistique.index') }}" class="nav-link text-white">Consulter les Statistiques</a>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('statistique2.index') }}" class="nav-link text-white">Connecter en tant que (ADMIN)</a>
+            <li id="li" class="nav-item dropdown">
+                <a class="btn btn-warning btn-lg nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->utilisateur }}
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="{{ route ('statistique2.index') }}">Connecter en tant que (ADMIN)</a></li>
+                    <li><a class="dropdown-item" href="{{ route ('login.show') }}">Se déconnecter</a></li>
+                </ul>
             </li>
-            <li class="nav-item nav-link">
-                <a href="{{ route ('login.show') }}" class="nav-link text-white">Se déconnecter</a>
-            </li>  
         </div>
     </nav>
+
+    <!-- Bootstrap 5 JavaScript dependencies -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
+<style>
+    #li{
+        margin-left: 450px;
+    }
+</style>
 
 
 
