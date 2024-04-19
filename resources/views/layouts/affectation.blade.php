ad6107fad6783c8e9478faa0a0a43c1f672ea381
MALEK16000 <dzmalek160@gmail.com>
2024-04-19 17:42:49 +0100

    chofo statistiques

--- resources/views/layouts/affectation.blade.php
+++ resources/views/layouts/affectation.blade.php
@@ -69,49 +69,6 @@
             window.history.back();
         }
     </script>
-    {{--<form action="{{ route('affectation.update',$affectation->id) }}" method="POST">
-    @csrf 
-    @method('PUT')
-    <div class="form-group">
-        <h5>Séléctionner le nom d'école :</h5>
-        <!--<input type="text" class="form-control" name="structure_iap_id" autocomplete="off"> -->
-        <select class="form-control" name="nom_ecole">
-            @foreach ($ecoles as $ecole)
-            <option @if(request()->has('modifier')) @if($ecole->id == $affectation->structure_iap_id) selected @endif @endif>{{$ecole->nom}}</option>
-            @endforeach
-          </select>
-    </div>
-    <br>
-    <div class="form-group">
-        <h5>Séléctionner le type :</h5>
-        <select class="form-control" name="type">
-            <option @if(request()->has('modifier')) @if($affectation->type == "direction") selected @endif @endif>direction</option>
-            <option @if(request()->has('modifier')) @if($affectation->type == "département") selected @endif @endif>département</option>
-          </select>    
-    </div>
-    <div class="form-group">
-        <h5>Nom :</h5>
-        <input type="text" class="form-control" name="nom" autocomplete="off" @if(request()->has('modifier')) value="{{$affectation->nom}}" @endif>
-        @error('nom')
-            <small class='text-danger'>{{$message}}</small>
-        @enderror
-    </div>
-    <div class="form-group">
-        <h5>Quota projet fin d'étude :</h5>
-        <input type="text" class="form-control" name="quota_pfe" autocomplete="off" @if(request()->has('modifier')) value="{{$affectation->quota_pfe}}" @endif>
-        @error('quota_pfe')
-            <small class='text-danger'>{{$message}}</small>
-        @enderror
-    </div>
-    <div class="form-group">
-        <h5>Quota immersion :</h5>
-        <input type="text" class="form-control" name="quota_im" autocomplete="off" @if(request()->has('modifier')) value="{{$affectation->quota_im}}" @endif>
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-success my-2" value="Enregistrer" name="modifier">
-        <input type="submit" class="btn btn-danger" value="Annuler" name="modifier">
-    </div>
-</form>--}}
     @else
     <form action="{{ route('affectation.store') }}" method="POST">
     @csrf
